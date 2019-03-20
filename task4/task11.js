71d8047211d48171b8fa269140d34077fbb87db1
Vitaly Korneev <t0taL90ryazan@gmail.com>
2019-03-20 17:29:01 +0300

    corrected commit

--- task4/task11.js
+++ task4/task11.js
@@ -1,19 +1,19 @@
 function count(str) {
 	var result = {};
 	for (var i = 0; i < str.length; i++) {
-        if (str[i] !== ' ') {
-            result[str[i]] = 0;
-        };
-    };
-    
+		if (str[i] !== ' ') {
+			result[str[i]] = 0;
+		};
+	};
+
 	for (var i = 0; i < str.length; i++) {
-        for (var key in result) {
-            if (key === str[i]) {
-                result[key]++;
-            };
-        };
-    };
-    
+		for (var key in result) {
+			if (key === str[i]) {
+				result[key]++;
+			};
+		};
+	};
+
 	for (var key in result) {
 		console.log(key + ': ' + result[key]);
 	};
