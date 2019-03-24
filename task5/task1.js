2760ccbf9dabb782a5fb6fd0cfd3c9fc4fe5529b
Vitaly Korneev <t0taL90ryazan@gmail.com>
2019-03-24 15:03:27 +0300

    corrected commit

--- task5/task1.js
+++ task5/task1.js
@@ -1,42 +1,34 @@
-var Calculator = {
-    result: 0,
-    add: function(num) {
-        if (typeof(num) !== 'undefined') {
-            Calculator.result += num;
-        };
-        return function(num) {
-            return Calculator.add(num);
-        };
-    },
-    subtract: function(num) {
-        if (typeof(num) !== 'undefined') {
-            Calculator.result -= num;  
-        };
-        return function(num) {
-            return Calculator.subtract(num);
-        };  
-    },
-    divide: function(num) {
-        if (typeof(num) !== 'undefined') {
-            Calculator.result /= num;
-        };
-        return function(num) {
-            return Calculator.divide(num);
-        };
-    },
-    multiply: function(num) {
-        if (typeof(num) !== 'undefined') {
-            Calculator.result *= num;
-        };
-        return function(num) {
-            return Calculator.multiply(num);
-        };
-    },
-    getResult: function() {
-        return Calculator.result;
-    },
-    reset: function() {
-        Calculator.result = 0;
-        return Calculator.result;
-    },
+var Calculator = new function() {
+  var result = 0;
+  this.add = function(num) {
+    if (num) result += num;
+    return function(num) {
+      return Calculator.add(num);
+    };
+  };
+  this.subtract = function(num) {
+    if (num) result -= num;
+    return function(num) {
+      return Calculator.subtract(num);
+    };
+  };
+  this.divide = function(num) {
+    if (num) result /= num;
+    return function(num) {
+      return Calculator.divide(num);
+    };
+  };
+  this.multiply = function(num) {
+    if (num) result *= num;
+    return function(num) {
+      return Calculator.multiply(num);
+    };
+  };
+  this.reset = function() {
+    result = 0;
+    return result;
+  };
+  this.getResult = function() {
+    return result;
+  };
 };
