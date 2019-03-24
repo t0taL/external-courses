2b30b2669fa7760d83a87bc4b9120f9bae4cec91
Vitaly Korneev <t0taL90ryazan@gmail.com>
2019-03-24 19:01:21 +0300

    corrected commit

--- task6/task6.js
+++ task6/task6.js
@@ -1,15 +1,9 @@
 function myReduce(arr, callback, initVal) {
-	var total;
-	if (initVal) {
-		total = initVal;
-		for (var i = 0; i < arr.length; i++) {
-			total = callback(total, arr[i], i, arr)
-		};
-	} else {
-		total = arr[0];
-		for (var i = 1; i < arr.length; i++) {
-			total = callback(total, arr[i], i, arr)
-		};
-	};
-	return total;
+  let total = 0;
+  if (arr[0]) total = arr[0];
+  if (initVal) total += initVal;
+  for (var i = 1; i < arr.length; i++) {
+    total = callback(total, arr[i], i, arr)
+  };
+  return total;
 };
