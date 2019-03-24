1fb30e59c3a7ceb9139a81e7cf2d966bdea10f57
Vitaly Korneev <t0taL90ryazan@gmail.com>
2019-03-24 15:10:22 +0300

    corrected commit

--- task5/task1.js
+++ task5/task1.js
@@ -1,27 +1,28 @@
 var Calculator = new function() {
-  var result = 0;
+  var result = 0,
+      target = this;
   this.add = function(num) {
     if (num) result += num;
     return function(num) {
-      return Calculator.add(num);
+      return target.add(num);
     };
   };
   this.subtract = function(num) {
     if (num) result -= num;
     return function(num) {
-      return Calculator.subtract(num);
+      return target.subtract(num);
     };
   };
   this.divide = function(num) {
     if (num) result /= num;
     return function(num) {
-      return Calculator.divide(num);
+      return target.divide(num);
     };
   };
   this.multiply = function(num) {
     if (num) result *= num;
     return function(num) {
-      return Calculator.multiply(num);
+      return target.multiply(num);
     };
   };
   this.reset = function() {
